b148ced9d59d71fd55688a0314a9748a35e7e8b3
MiaoMint <1981324730@qq.com>
2023-08-29 13:48:29 +0800

    Fix: default data dir error

--- config/env.go
+++ config/env.go
@@ -2,12 +2,12 @@ package config
 
 import "os"
 
-const VERSION = "0.0.1"
+const VERSION = "0.0.2"
 
 var (
 	SECRET                 = getVar("SECRET", "")
 	SERVER_LISTEN          = getVar("SERVER_LISTEN", "127.0.0.1:3000")
-	DATA_DIR               = getVar("DATA_DIR", "./bg-server-data")
+	DATA_DIR               = getVar("DATA_DIR", "./bt-server-data")
 	AUTO_DELETE_CACHE_FILE = getVar("AUTO_DELETE_CACHE_FILE", "true")
 )
 
